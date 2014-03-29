d7b010265fbd2479b1d83330ee0d5a92423ec590
Roman Kierzkowski <roman.kierzkowski@stxnext.pl>
2014-03-29 12:14:38 +0100

    Moved from switzerland-admin-example.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='amber-lib',
+    version='0.1',
+    packages=['amber',],
+    license='Other/Proprietary License',
+    long_description=open('README.md').read(),
+)
