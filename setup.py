9cef3686dc19a44b58550553bc8ed9e25fb56530
Roman Kierzkowski <roman.kierzkowski@stxnext.pl>
2014-03-31 15:04:31 +0200

    New namming.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='amber-lib',
     version='0.1',
-    packages=['amber',],
+    packages=['amber_lib',],
     license='Other/Proprietary License',
     long_description=open('README.md').read(),
 )
