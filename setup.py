c1635730dfbb3af33896325bed5e24acfc319d64
Garnef <chriscastle@doodlehome.com>
2015-01-28 16:03:07 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='amber-lib',
-    version='0.5',
+    version='0.6',
     packages=['amber_lib'],
     license='Other/Proprietary License',
     long_description=open('README.md').read(),
