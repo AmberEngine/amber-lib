bd1a9d70c4b0d3e87ef464c1c95786f002fb267f
apjoye <turbomega7@gmail.com>
2015-02-10 15:37:58 -0500

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='amber-lib',
-    version='0.8',
+    version='0.9',
     packages=['amber_lib'],
     license='Other/Proprietary License',
     long_description=open('README.md').read(),
