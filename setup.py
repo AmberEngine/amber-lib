a71afd7d68dcbd001bf4df199488ee281c2136ec
Garnef <chris@amberengine.com>
2015-02-25 18:21:05 -0500

    Added method for product listing get

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='amber-lib',
-    version='0.11',
+    version='0.12',
     packages=['amber_lib'],
     license='Other/Proprietary License',
     long_description=open('README.md').read(),
