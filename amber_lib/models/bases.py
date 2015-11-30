5ae10845fe072aba712a40c882ba03bd741dffa4
= <curtis@amberengine.com>
2015-11-30 15:32:43 -0500

    Pep8 the bases file

--- amber_lib/models/bases.py
+++ amber_lib/models/bases.py
@@ -3,13 +3,13 @@ import json
 
 class Model(object):
     _ctx = None
+    attributes = {}
 
     def __init__(self, context):
         """ Initialize a new instance of the Model, saving the current context
         internally.
         """
         self._ctx = context
-
 
     def __getattr__(self, attr):
         """ If the desired attribute exists, return the current value that it
@@ -20,7 +20,6 @@ class Model(object):
         else:
             raise Exception('NOPE NOPE NOPE!')
 
-
     def __setattr__(self, attr, val):
         """ If the desired attribute exists, set its internal value using
         the set method to that of the specified value.
@@ -29,7 +28,6 @@ class Model(object):
             self.attributes[attr].set(val)
         else:
             raise Exception('nope!')
-
 
     def all(self, limit=500, offset=0):
         """ Retrieve a collection of instances of the model, using the
@@ -44,13 +42,19 @@ class Model(object):
         """ Update the internal dictionary for the instance using the
         key-value pairs contained within the provided dictionary.
         """
-        for key, val in dict_.iteritems():
-            if key not in self.__dict__.keys():
-                raise Exception("That key dont exist, yo!")
-            else:
-                self.__dict__[key] = val
+        def explodeDict(obj, dict_):
+            for key, val in dict_.items():
+                if key not in obj.attributes:
+                    raise Exception("yo! That dont work!")
 
-        return self
+                if isinstance(val, [dict]):
+                    dict_ = val
+                    inst = getattr(amberlib.models.components, key)(self._ctx)
+
+                obj.attributes[key].set(val)
+            return obj
+
+        return explodeDict(self, dict_)
 
     def fromJSON(self, json):
         """ Update the internal dictionary for the instance using the
@@ -69,13 +73,21 @@ class Model(object):
             self.update(data)
 
         if isinstance(self.id, int) and self.id > 0:
-            returnedDict = amberlib.Put(self.ctx, self.__class__, self.toDict(), id=self.id)
+            returnedDict = amberlib.Put(
+                self.ctx,
+                self.__class__,
+                self.toDict(),
+                id=self.id
+            )
         else:
-            returnedDict = amberlib.Post(self.ctx, self.__class__, self.toDict())
+            returnedDict = amberlib.Post(
+                self.ctx,
+                self.__class__,
+                self.toDict()
+            )
 
         self.update(returned)
         return self
-
 
     def toDict(self):
         """ Retrieve a dictionary version of the model.
@@ -92,13 +104,11 @@ class Model(object):
 
         return dictionary
 
-
     def toJSON(self):
         """ Retrieve the model as a JSON object string, containing
         key-value pairs for the internal class data.
         """
         return json.dumps(self.toDict())
-
 
     def update(self, data):
         """ Update the internal data of the class instance using either
@@ -121,6 +131,20 @@ class Model(object):
 
         return self
 
+
+class Property:
+    def __init__(self, kind):
+        self.kind = kind
+        self.value = None
+
+    def get(self):
+        return self.value
+
+    def set(self, value):
+        if isinstance(value, [self.kind]):
+            self.value = value
+        else:
+            raise Exception('can set that!')
 
 
 class Component(Model):
