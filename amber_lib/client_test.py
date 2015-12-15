a9c70abef5c000818ddb4a5b46c42c6d8fbeaefb
= <curtis@amberengine.com>
2015-12-15 18:31:07 -0500

    Alpha-organized client test

--- amber_lib/client_test.py
+++ amber_lib/client_test.py
@@ -6,21 +6,12 @@ import sys
 
 import mock
 
-from .models import bases
-from . import client
-
-
-class Context(object):
-    public = 'mwyclv6bac2lqn9artt1o88laq4muk88483opkp9dnv8289f8olqrxlx7b2s7q8'
-    private = "sn4ikxumkpi5dqb0vwo1ujbi68uv3bvoak1p0xzgbzhg71v9p1sn7a2t49dh2tz"
-    host = "http://example.com"
-    port = "8080"
-    request_attempts = 3
+from amber_lib import client
+from amber_lib.models import bases
 
 
 FAKE_DATE = datetime(2015, 11, 30, 22, 36, 52, 538755)
 FAKE_DATE_FORMAT = datetime.isoformat(FAKE_DATE)
-
 FAKE_HAL = {
     "_embedded": {
         "models": [
@@ -45,8 +36,54 @@ FAKE_HAL_THREE = {
 }
 
 
-class ContainerTest(unittest.TestCase):
-    def add_test(self):
+class Context(object):
+    public = 'mwyclv6bac2lqn9artt1o88laq4muk88483opkp9dnv8289f8olqrxlx7b2s7q8'
+    private = "sn4ikxumkpi5dqb0vwo1ujbi68uv3bvoak1p0xzgbzhg71v9p1sn7a2t49dh2tz"
+    host = "http://example.com"
+    port = "8080"
+    request_attempts = 3
+
+
+class Container(unittest.TestCase):
+    def all_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        self.assertEqual(ctn[:].values, ctn._Container__all().values)
+
+    def append_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        model = bases.Model(Context())
+
+        before = len(ctn)
+        ctn.append(model)
+
+        self.assertEqual(len(ctn), before + 1)
+        self.assertEqual(ctn[-1], model)
+
+    def count_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        self.assertEqual(ctn.count(), len(ctn))
+
+    def delete_invalid_index_test(self):
+        ctn = client.Container(FAKE_HAL, bases.Model, Context())
+
+        def delete_index():
+            ctn.remove("foobar")
+        self.assertRaises(ValueError, delete_index)
+
+    def delete_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        second = ctn[1]
+
+        ctn.remove(second)
+
+        self.assertEqual(len(ctn), 2)
+        self.assertEqual(ctn[1]._links, "third")
+        self.assertNotIn(second, ctn)
+
+    def dunder_add_test(self):
         hal = {
             "_embedded": {
                 "models": [
@@ -68,11 +105,39 @@ class ContainerTest(unittest.TestCase):
         self.assertEqual(len(ctn3.values), 3)
         self.assertEqual(ctn3[-1]._links, "third")
 
-    def add_wrong_type_test(self):
+    def dunder_add_wrong_type_test(self):
         ctn1 = client.Container(FAKE_HAL, bases.Model, Context())
         self.assertRaises(TypeError, lambda: ctn1 + "foobar")
 
-    def contains_test(self):
+    def dunder_append_container_test(self):
+        ctn1 = client.Container(FAKE_HAL, bases.Model, Context())
+        ctn2 = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        ctn3 = client.Container({}, bases.Model, Context())
+
+        expected = len(ctn1) + len(ctn2) + len(ctn3)
+
+        ctn1._Container__append(ctn2)
+        ctn1._Container__append(ctn3)
+        self.assertEqual(len(ctn1), expected)
+
+    def dunder_append_item_test(self):
+        ctn = client.Container(FAKE_HAL, bases.Model, Context())
+
+        expected = len(ctn) + 1
+
+        ctn._Container__append(bases.Model({}))
+        self.assertEqual(len(ctn), expected)
+
+    def dunder_append_list_test(self):
+        ctn = client.Container(FAKE_HAL, bases.Model, Context())
+        models = [bases.Model({}), bases.Model({})]
+
+        expected = len(ctn) + len(models)
+
+        ctn._Container__append(models)
+        self.assertEqual(len(ctn), expected)
+
+    def dunder_contains_test(self):
         ctn = client.Container(FAKE_HAL, bases.Model, Context())
         model = ctn[0]
         self.assertIn(model, ctn)
@@ -96,159 +161,20 @@ class ContainerTest(unittest.TestCase):
             del ctn[100]
         self.assertRaises(IndexError, delete_index)
 
-    def dunder_getitem_slice_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        self.assertEqual(len(ctn[:]), 3)
-        self.assertEqual(len(ctn[0:2]), 2)
-        self.assertEqual(len(ctn[0:3:2]), 2)
-
-        self.assertEqual(ctn[0:2][1]._links, "second")
-
-    def dunder_getitem_invalid_slice_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-
-        self.assertEqual(len(ctn[1:500]), 2)
-        self.assertEqual(len(ctn[100:1001]), 0)
-
-    def dunder_getitem_index_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-
-        self.assertEqual(ctn[0]._links, "first")
-        self.assertEqual(ctn[1]._links, "second")
-        self.assertEqual(ctn[-1]._links, "third")
-
-    def dunder_getitem_invalid_index_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-
-        self.assertRaises(IndexError, lambda: ctn[42])
-        self.assertRaises(IndexError, lambda: ctn[-99])
-
-    def dunder_getitem_invalid_index_type_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-
-        self.assertRaises(TypeError, lambda: ctn["one"])
-        self.assertRaises(TypeError, lambda: ctn[3.14159])
-
-    def dunder_iter_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-
-        values = ctn.values
-        count = 0
-
-        for item in ctn:
-            self.assertEqual(values[count], item)
-            count += 1
-
-        self.assertEqual(count, len(ctn))
-
-    def dunder_iter_no_items_test(self):
-        ctn = client.Container({}, bases.Model, Context())
-        count = 0
-
-        for item in ctn:
-            count += 1
-
-        self.assertEqual(count, 0)
-
-    def le_api_test(self):
-        ctn = client.Container({}, bases.Model, Context())
-        self.assertTrue((ctn <= ctn).startswith("le"))
-
-    def len_total_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        self.assertEqual(len(ctn), 3)
-
-    def len_empty_test(self):
-        ctn = client.Container({}, bases.Model, Context())
-        self.assertEqual(len(ctn), 0)
-
-    def len_values_test(self):
-        ctn = client.Container({}, bases.Model, Context())
-
-        class Length:
-            def __len__(self):
-                return 42
-
-        ctn.values = Length()
-        self.assertEqual(len(ctn), 42)
-
-    def pow_test(self):
-        ctn = client.Container({}, bases.Model, Context())
-        self.assertTrue('Mario' in ctn ** 42)
-
-    def reversed_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        values = ctn[:]
-
-        reversed_ctn = reversed(ctn)
-        for i in range(len(reversed_ctn)):
-            self.assertEqual(reversed_ctn[i], values[len(values) - i - 1])
-
-    def setitem_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        new_model = bases.Model({})
-        new_model._links = "fourth"
-
-        ctn[2] = new_model
-
-        self.assertEqual(len(ctn), 3)
-        self.assertEqual(ctn[-1]._links, "fourth")
-
-    def setitem_invalid_range_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        new_model = bases.Model({})
-        new_model._links = "fourth"
-
-        def temp():
-            ctn[42] = new_model
-        self.assertRaises(IndexError, temp)
-
-    def all_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        self.assertEqual(ctn[:].values, ctn._Container__all().values)
-
-    def dunder_append_container_test(self):
-        ctn1 = client.Container(FAKE_HAL, bases.Model, Context())
-        ctn2 = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        ctn3 = client.Container({}, bases.Model, Context())
-
-        expected = len(ctn1) + len(ctn2) + len(ctn3)
-
-        ctn1._Container__append(ctn2)
-        ctn1._Container__append(ctn3)
-        self.assertEqual(len(ctn1), expected)
-
-    def dunder_append_list_test(self):
+    @mock.patch('amber_lib.client.Container._Container__all')
+    def dunder_finish_it_ignored_test(self, mock_all):
         ctn = client.Container(FAKE_HAL, bases.Model, Context())
-        models = [bases.Model({}), bases.Model({})]
+        ctn._Container__finish_it()
 
-        expected = len(ctn) + len(models)
-
-        ctn._Container__append(models)
-        self.assertEqual(len(ctn), expected)
-
-    def dunder_append_item_test(self):
-        ctn = client.Container(FAKE_HAL, bases.Model, Context())
-
-        expected = len(ctn) + 1
-
-        ctn._Container__append(bases.Model({}))
-        self.assertEqual(len(ctn), expected)
+        mock_all.assert_not_called()
 
     @mock.patch('amber_lib.client.Container._Container__all')
-    def finish_it_test(self, mock_all):
+    def dunder_finish_it_test(self, mock_all):
         ctn = client.Container(FAKE_HAL, bases.Model, Context())
         ctn.total = 500
         ctn._Container__finish_it()
 
         mock_all.assert_called_with()
-
-    @mock.patch('amber_lib.client.Container._Container__all')
-    def finish_it_ignored_test(self, mock_all):
-        ctn = client.Container(FAKE_HAL, bases.Model, Context())
-        ctn._Container__finish_it()
-
-        mock_all.assert_not_called()
 
     def dunder_get_invalid_negative_index_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
@@ -257,12 +183,6 @@ class ContainerTest(unittest.TestCase):
             IndexError,
             lambda: ctn._Container__get(too_far_under)
         )
-
-    def dunder_get_valid_negative_index_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        model = ctn[2]
-
-        self.assertEqual(ctn._Container__get(-1), model)
 
     def dunder_get_invalid_positive_index_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
@@ -308,6 +228,66 @@ class ContainerTest(unittest.TestCase):
 
         self.assertEqual(ctn._Container__get(0), "first index")
         mock_previous.assert_called_with()
+
+    def dunder_get_valid_negative_index_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        model = ctn[2]
+
+        self.assertEqual(ctn._Container__get(-1), model)
+
+    def dunder_getitem_index_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        self.assertEqual(ctn[0]._links, "first")
+        self.assertEqual(ctn[1]._links, "second")
+        self.assertEqual(ctn[-1]._links, "third")
+
+    def dunder_getitem_invalid_index_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        self.assertRaises(IndexError, lambda: ctn[42])
+        self.assertRaises(IndexError, lambda: ctn[-99])
+
+    def dunder_getitem_invalid_index_type_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        self.assertRaises(TypeError, lambda: ctn["one"])
+        self.assertRaises(TypeError, lambda: ctn[3.14159])
+
+    def dunder_getitem_invalid_slice_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        self.assertEqual(len(ctn[1:500]), 2)
+        self.assertEqual(len(ctn[100:1001]), 0)
+
+    def dunder_getitem_slice_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        self.assertEqual(len(ctn[:]), 3)
+        self.assertEqual(len(ctn[0:2]), 2)
+        self.assertEqual(len(ctn[0:3:2]), 2)
+
+        self.assertEqual(ctn[0:2][1]._links, "second")
+
+    def dunder_iter_no_items_test(self):
+        ctn = client.Container({}, bases.Model, Context())
+        count = 0
+
+        for item in ctn:
+            count += 1
+
+        self.assertEqual(count, 0)
+
+    def dunder_iter_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        values = ctn.values
+        count = 0
+
+        for item in ctn:
+            self.assertEqual(values[count], item)
+            count += 1
+
+        self.assertEqual(count, len(ctn))
 
     def dunder_next_no_hal_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
@@ -387,21 +367,24 @@ class ContainerTest(unittest.TestCase):
         self.assertTrue(mock_send.called)
         mock_prepend.assert_called_with(embedded['models'])
 
-    def append_test(self):
+    def dunder_setitem_invalid_range_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        new_model = bases.Model({})
+        new_model._links = "fourth"
 
-        model = bases.Model(Context())
+        def temp():
+            ctn[42] = new_model
+        self.assertRaises(IndexError, temp)
 
-        before = len(ctn)
-        ctn.append(model)
-
-        self.assertEqual(len(ctn), before + 1)
-        self.assertEqual(ctn[-1], model)
-
-    def count_test(self):
+    def dunder_setitem_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        new_model = bases.Model({})
+        new_model._links = "fourth"
 
-        self.assertEqual(ctn.count(), len(ctn))
+        ctn[2] = new_model
+
+        self.assertEqual(len(ctn), 3)
+        self.assertEqual(ctn[-1]._links, "fourth")
 
     def extend_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
@@ -414,16 +397,16 @@ class ContainerTest(unittest.TestCase):
         self.assertEqual(len(ctn), before + 1)
         self.assertEqual(ctn[-1], model)
 
+    def index_non_existant_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+
+        self.assertRaises(ValueError, lambda: ctn.index("foobar"))
+
     def index_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
         index = 1
 
         self.assertEqual(ctn.index(ctn[index]), index)
-
-    def index_non_existant_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-
-        self.assertRaises(ValueError, lambda: ctn.index("foobar"))
 
     def insert_test(self):
         ctn = client.Container(FAKE_HAL, bases.Model, Context())
@@ -437,12 +420,27 @@ class ContainerTest(unittest.TestCase):
         self.assertEqual(ctn[1], model)
         self.assertEqual(ctn[2], second)
 
-    def pop_test(self):
-        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
-        length = len(ctn)
+    def le_api_test(self):
+        ctn = client.Container({}, bases.Model, Context())
+        self.assertTrue((ctn <= ctn).startswith("le"))
 
-        ctn.pop()
-        self.assertEqual(len(ctn), length - 1)
+    def len_empty_test(self):
+        ctn = client.Container({}, bases.Model, Context())
+        self.assertEqual(len(ctn), 0)
+
+    def len_total_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        self.assertEqual(len(ctn), 3)
+
+    def len_values_test(self):
+        ctn = client.Container({}, bases.Model, Context())
+
+        class Length:
+            def __len__(self):
+                return 42
+
+        ctn.values = Length()
+        self.assertEqual(len(ctn), 42)
 
     def pop_index_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
@@ -459,23 +457,16 @@ class ContainerTest(unittest.TestCase):
 
         self.assertRaises(IndexError, lambda: ctn[2])
 
-    def delete_test(self):
+    def pop_test(self):
         ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        length = len(ctn)
 
-        second = ctn[1]
+        ctn.pop()
+        self.assertEqual(len(ctn), length - 1)
 
-        ctn.remove(second)
-
-        self.assertEqual(len(ctn), 2)
-        self.assertEqual(ctn[1]._links, "third")
-        self.assertNotIn(second, ctn)
-
-    def delete_invalid_index_test(self):
-        ctn = client.Container(FAKE_HAL, bases.Model, Context())
-
-        def delete_index():
-            ctn.remove("foobar")
-        self.assertRaises(ValueError, delete_index)
+    def pow_test(self):
+        ctn = client.Container({}, bases.Model, Context())
+        self.assertTrue('Mario' in ctn ** 42)
 
     def reverse_test(self):
         ctn = client.Container(FAKE_HAL, bases.Model, Context())
@@ -483,6 +474,14 @@ class ContainerTest(unittest.TestCase):
         ctn.reverse()
 
         self.assertEqual(reversed_ctn.values, ctn.values)
+
+    def reversed_test(self):
+        ctn = client.Container(FAKE_HAL_THREE, bases.Model, Context())
+        values = ctn[:]
+
+        reversed_ctn = reversed(ctn)
+        for i in range(len(reversed_ctn)):
+            self.assertEqual(reversed_ctn[i], values[len(values) - i - 1])
 
 
 class CreatePayload(unittest.TestCase):
@@ -554,7 +553,6 @@ class Send(unittest.TestCase):
             AttributeError,
             lambda: client.send(method, Context(), endpoint, {})
         )
-
 
 
 if __name__ == "__main__":
