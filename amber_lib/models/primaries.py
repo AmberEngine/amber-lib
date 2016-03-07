33922e377f7825b24bd476d01bcc6ea6fb4053d3
ryan <ryan@amberengine.com>
2016-03-07 16:17:17 -0500

    manufacturer.internal str > bool

--- amber_lib/models/primaries.py
+++ amber_lib/models/primaries.py
@@ -101,7 +101,7 @@ class Manufacturer(Model):
     featured = Property(bool)
     google_plus_url = Property(str)
     id = Property(int)
-    internal = Property(str)
+    internal = Property(bool)
     legal = Property(str)
     linkedin_url = Property(str)
     logo_url = Property(str)
