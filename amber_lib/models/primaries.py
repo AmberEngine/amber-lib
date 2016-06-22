2c8adef5f4ad7af523191eb1d02d6f9df56a5882
Joe Kari <joe@amberengine.com>
2016-06-22 13:44:56 -0400

    test

--- amber_lib/models/primaries.py
+++ amber_lib/models/primaries.py
@@ -1,7 +1,7 @@
 from amber_lib.models.bases import Model, Property, resource
 from amber_lib import client
 from amber_lib.errors import MethodNotAllowed
-import amber_lib.models.components as components
+from amber_lib.models import components
 
 @resource('api_keys')
 class APIKey(Model):
