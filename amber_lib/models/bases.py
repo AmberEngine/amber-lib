a1d6b23ff54faf4da2acfb2329708c1a77a3f968
Joe Kari <joe@amberengine.com>
2016-09-12 16:31:46 -0400

    testing

--- amber_lib/models/bases.py
+++ amber_lib/models/bases.py
@@ -195,7 +195,7 @@ class Model(object):
 
                 if isinstance(val, dict):
                     if not isinstance(attr, dict):
-                        print(attr.kind())
+                        print(attr.kind(obj.ctx()))
                         inst = attr.kind(obj.ctx())
                         # Try to fill the new instance with data from the old
                         # instance so we don't lose data that isn't included
