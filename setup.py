edfa46f2e216ca12a8ed0347df1b9e49fed5af99
jg75 <james@amberengine.com>
2016-11-23 12:19:46 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='amber-lib',
-    version='1.4',
+    version='1.5.1',
     packages=['amber_lib'],
     license='Other/Proprietary License',
     long_description=open('README.md').read(),
