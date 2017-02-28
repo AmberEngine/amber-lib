ab0fbba2dbf81db595bb17594e02e6b26750e36f
Curtis La Graff <curtis@amberengine.com>
2017-02-28 15:39:15 -0500

    fubar v8
    Signed-off-by: Curtis La Graff <curtis@amberengine.com>

--- amber_lib/resources.py
+++ amber_lib/resources.py
@@ -109,7 +109,8 @@ class BaseResource(object):
 
 
 class EmbeddedList(list):
-    __pk_field = ""
+    #__pk_field = ""
+    """
     def __init__(self, type_=None, *args, **kwargs):
         if type_ == "products":
             self.__pk_field = "guid"
@@ -117,20 +118,21 @@ class EmbeddedList(list):
             self.__pk_field == "id"
 
         self.__id_mapping = {} # contains pk->index pairs
-        #super().__init__(*args, **kwargs)
-
+        super().__init__(*args, **kwargs)
+    """
 
     def __setitem__(self, key, value):
         super().__setitem__(key, value)
-        self.__id_mapping[getattr(value, self.__pk_field)] = key
+        #self.__id_mapping[getattr(value, self.__pk_field)] = key
 
     def __delitem__(self, key):
         v = self[key]
         super().__delitem__(key)
-        del self.__id_mapping[getattr(v, self.__pk_field)]
+        #del self.__id_mapping[getattr(v, self.__pk_field)]
 
     def pk(self, id_):
-        return self[self.__id_mapping[id_]]
+        print("shit")
+        #return self[self.__id_mapping[id_]]
 
 def create_url(context, endpoint, **uri_args):
     """ Create a full URL using the provided components."""
